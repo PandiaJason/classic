f723434141d59cd7e06450257c73d8538219f267
PandiaJason <pandiajason@gmail.com>
2026-05-06 09:44:25 +0530

    Splash: reduced to 1.5 seconds

--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	# Show for 5 seconds then go to menu
 	var timer = Timer.new()
-	timer.wait_time = 5.0
+	timer.wait_time = 1.5
 	timer.one_shot = true
 	timer.timeout.connect(_go_to_menu)
 	add_child(timer)
